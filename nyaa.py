d6e38e995e5265055aca251122eb5ee9ac508fea
Pongor Ádám <93447735+ipowi01@users.noreply.github.com>
2025-03-15 21:43:44 +0100

    Add files via upload

---
+++ nyaa.py
@@ -0,0 +1,11 @@
+import re
+def x():
+    if p=="help":
+        print("0=angol\n1=magyar\nWork in progress")
+    if p=="0":
+        y=re.sub("[rl]","w", nya)
+        y=re.sub("\.", ". uwu nya :3", y)
+        print(y)
+p=input("Paraméter")
+nya=input("Szöveg:")
+x()
